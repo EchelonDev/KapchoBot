2e40208b728a0c60ab1d7380f6d5a7e20d251699
bgrcdnc <nonfiction178@gmail.com>
2016-03-11 16:47:20 +0200

    Added "!dealwithit"

--- discord_bot.js
+++ discord_bot.js
@@ -1653,6 +1653,7 @@ var caps = {
     "adeyisd": "adeyisd.jpg",
     "dickbutt": "dickbutt.jpg",
     "bangif": "ban.gif",
+    "dealwithit": "dealwithit.gif",
     "bravo": {
         path: "nonfic/",
         name: "bravo.jpg"
